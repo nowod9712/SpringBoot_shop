8147775661afc0c9afcae967e4e89d047ede796a
nowod <ldowon1223@naver.com>
2024-04-01 11:40:02 +0900

    쿼리메소드/ @애너테이션 쿼리를 이용한 테스트 진행

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -1,8 +1,11 @@
 package com.shop;
 
+import com.shop.entity.Item;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ShopApplication {
@@ -10,5 +13,8 @@ public class ShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
 	}
+
+
+
 
 }
--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -1,6 +1,5 @@
 package com.shop.entity;
 
-
 import com.shop.constant.ItemSellStatus;
 import jakarta.persistence.*;
 import lombok.*;
@@ -8,7 +7,6 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name="item") //클래스의 이름과 테이블의 이름이 같으면 생략, 다르면 테이블명 작성
 @Getter
 @Setter
 @ToString
@@ -17,26 +15,26 @@ public class Item {
     @Id
     @Column(name="item_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // 상품코드
+    private Long id;          //상품 코드
 
     @Column(nullable = false, length = 50)
-    private String itemNm; // 상품명
+    private String itemNm;  // 상품명
 
     @Column(nullable = false)
-    private int price; // 가격
+    private int price;      // 가격
 
     @Column(nullable = false)
     private int stockNumber; // 재고수량
 
-    @Lob //"Lob"은 "Large Object"의 약자로, 대용량의 데이터를 의미합니다.
+    @Lob
     @Column(nullable = false)
     private String itemDetail; // 상품 상세 설명
 
-    @Enumerated(EnumType.STRING) //열거형 상수의 문자열 값이 데이터베이스에 저장
+    @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus; // 상품 판매 상태
 
-    private LocalDateTime regTime; // 등록시간
+    private LocalDateTime regTime; // 등록 시간
 
-    private LocalDateTime updateTime; // 수정시간
-    
+    private LocalDateTime updateTime; // 수정 시간
+
 }
